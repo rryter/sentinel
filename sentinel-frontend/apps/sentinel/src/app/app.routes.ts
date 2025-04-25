3656e728f8ba49566537c9a43864b745abd090cf
Reto Ryter <reto@twy.gmbh>
2025-04-25 14:49:37 +0000

    Refactor app routes to nest project-related paths and update redirect to projects

--- sentinel-frontend/apps/sentinel/src/app/app.routes.ts
+++ sentinel-frontend/apps/sentinel/src/app/app.routes.ts
@@ -7,27 +7,33 @@ export const appRoutes: Route[] = [
     component: GitHubCallbackComponent,
   },
   {
-    path: 'linting',
-    loadChildren: () =>
-      import('@sentinel/linting').then((m) => m.lintingRoutes),
-  },
-  {
     path: 'projects',
     loadChildren: () =>
       import('@sentinel/projects').then((m) => m.projectsRoutes),
   },
   {
-    path: 'builds',
-    loadChildren: () => import('@sentinel/build').then((m) => m.buildRoutes),
-  },
-  {
-    path: 'settings',
-    loadChildren: () =>
-      import('@sentinel/settings').then((m) => m.settingsRoutes),
+    path: 'projects/:projectId',
+    children: [
+      {
+        path: 'linting',
+        loadChildren: () =>
+          import('@sentinel/linting').then((m) => m.lintingRoutes),
+      },
+      {
+        path: 'builds',
+        loadChildren: () =>
+          import('@sentinel/build').then((m) => m.buildRoutes),
+      },
+      {
+        path: 'settings',
+        loadChildren: () =>
+          import('@sentinel/settings').then((m) => m.settingsRoutes),
+      },
+    ],
   },
   {
     path: '',
-    redirectTo: '/linting',
+    redirectTo: '/projects',
     pathMatch: 'full',
   },
 ];
