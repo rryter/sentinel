03d5b8b3b63e00445049dc639979fb229ba2431d
Reto Ryter <reto@twy.gmbh>
2025-04-28 13:49:57 +0000

    refactor(migration): simplify removal of metadata_content column from violations

--- sentinel-backend/db/migrate/20250428125848_remove_metadata_content_from_violations.rb
+++ sentinel-backend/db/migrate/20250428125848_remove_metadata_content_from_violations.rb
@@ -1,34 +1,5 @@
 class RemoveMetadataContentFromViolations < ActiveRecord::Migration[8.0]
-  def up
-    # First add the new jsonb column if it doesn't exist
-    unless column_exists?(:violations, :metadata)
-      add_column :violations, :metadata, :jsonb
-    end
-
-    # Migrate existing data from metadata_content to metadata
-    execute(<<-SQL)
-      UPDATE violations 
-      SET metadata = metadata_content::jsonb
-      WHERE metadata_content IS NOT NULL 
-        AND metadata IS NULL;
-    SQL
-
-    # Remove the old column
+  def change
     remove_column :violations, :metadata_content
-  end
-
-  def down
-    # Add back the metadata_content column
-    add_column :violations, :metadata_content, :text
-
-    # Restore data from metadata to metadata_content
-    execute(<<-SQL)
-      UPDATE violations 
-      SET metadata_content = metadata::text
-      WHERE metadata IS NOT NULL;
-    SQL
-
-    # Remove the metadata column
-    remove_column :violations, :metadata
   end
 end
