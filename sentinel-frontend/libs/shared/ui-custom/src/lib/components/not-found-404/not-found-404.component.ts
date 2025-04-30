8a46504570a7efe6cb3d3b76d5bbcea95a110821
Reto Ryter <reto@twy.gmbh>
2025-04-30 15:57:14 +0000

    feat: implement NotFound404 component with button for navigation and add styling

--- sentinel-frontend/libs/shared/ui-custom/src/lib/components/not-found-404/not-found-404.component.ts
+++ sentinel-frontend/libs/shared/ui-custom/src/lib/components/not-found-404/not-found-404.component.ts
@@ -1,10 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { HlmButtonDirective } from '@spartan-ng/ui-button-helm';
 
 @Component({
   selector: 'saas-not-found-404',
-  imports: [CommonModule, RouterLink],
+  imports: [CommonModule, RouterLink, HlmButtonDirective],
   templateUrl: './not-found-404.component.html',
   styleUrl: './not-found-404.component.css',
 })
