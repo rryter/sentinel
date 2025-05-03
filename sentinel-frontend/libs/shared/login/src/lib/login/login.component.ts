c7effa0978650e355e31958ac1b9159485d58ad2
Reto Ryter <reto@twy.gmbh>
2025-05-03 05:38:50 +0000

    fix: refactor WebAuthn login process to use async/await and remove unused animations

--- sentinel-frontend/libs/shared/login/src/lib/login/login.component.ts
+++ sentinel-frontend/libs/shared/login/src/lib/login/login.component.ts
@@ -1,11 +1,3 @@
-import {
-  animate,
-  query,
-  stagger,
-  style,
-  transition,
-  trigger,
-} from '@angular/animations';
 import { CommonModule } from '@angular/common';
 import { Component, inject } from '@angular/core';
 import {
@@ -16,13 +8,11 @@ import {
 import { Router, RouterLink } from '@angular/router';
 import { NgIcon, provideIcons } from '@ng-icons/core';
 import { lucideFingerprint, lucideGithub, lucideMail } from '@ng-icons/lucide';
-import {
-  PublicKeyCredentialRequestOptionsJSON,
-  startAuthentication,
-} from '@simplewebauthn/browser';
+import { startAuthentication } from '@simplewebauthn/browser';
 import { HlmButtonDirective } from '@spartan-ng/ui-button-helm';
 import { HlmFormFieldModule } from '@spartan-ng/ui-formfield-helm';
 import { HlmInputDirective } from '@spartan-ng/ui-input-helm';
+import { firstValueFrom } from 'rxjs';
 import { AuthService } from '../services/auth.service';
 
 @Component({
@@ -39,34 +29,6 @@ import { AuthService } from '../services/auth.service';
   ],
   providers: [provideIcons({ lucideFingerprint, lucideMail, lucideGithub })],
   templateUrl: './login.component.html',
-  animations: [
-    trigger('fadeInUp', [
-      transition(':enter', [
-        style({ opacity: 0, transform: 'translateY(10px)' }),
-        animate(
-          '0.5s ease-out',
-          style({ opacity: 1, transform: 'translateY(0)' }),
-        ),
-      ]),
-    ]),
-    trigger('staggerFade', [
-      transition('* => *', [
-        query(
-          ':enter',
-          [
-            style({ opacity: 0, transform: 'translateY(10px)' }),
-            stagger('100ms', [
-              animate(
-                '0.5s ease-out',
-                style({ opacity: 1, transform: 'translateY(0)' }),
-              ),
-            ]),
-          ],
-          { optional: true },
-        ),
-      ]),
-    ]),
-  ],
 })
 export class LoginComponent {
   private _formBuilder = inject(NonNullableFormBuilder);
@@ -90,40 +52,27 @@ export class LoginComponent {
 
     try {
       // Get authentication options from server
-      this.authService
-        .getWebAuthnSignInOptions(this.form.value.email)
-        .subscribe({
-          next: async (optionsJSON: PublicKeyCredentialRequestOptionsJSON) => {
-            // Start the WebAuthn authentication process
-            try {
-              console.log(optionsJSON);
-              const authResp = await startAuthentication({ optionsJSON });
+      const optionsJSON = await firstValueFrom(
+        this.authService.getWebAuthnSignInOptions(this.form.value.email),
+      );
 
-              // Send the assertion to the server for verification
-              this.authService.verifyWebAuthnSignIn(authResp).subscribe({
-                next: (response) => {
-                  console.log('Authentication successful:', response);
-                  // Navigate to the dashboard or home page
-                  this.router.navigate(['/']);
-                },
-                error: (err) => {
-                  console.error('Authentication verification failed:', err);
-                },
-              });
-            } catch (error: any) {
-              if (error.name === 'NotAllowedError') {
-                console.error('User declined the authentication request');
-              } else {
-                console.error('WebAuthn authentication error:', error);
-              }
-            }
-          },
-          error: (err) => {
-            console.error('Error getting authentication options:', err);
-          },
-        });
-    } catch (error) {
-      console.error('WebAuthn authentication error:', error);
+      // Start the WebAuthn authentication process
+      const authResp = await startAuthentication({ optionsJSON });
+
+      // Send the assertion to the server for verification
+      const response = await firstValueFrom(
+        this.authService.verifyWebAuthnSignIn(authResp),
+      );
+
+      console.log('Authentication successful:', response);
+      // Navigate to the dashboard or home page
+      this.router.navigate(['/']);
+    } catch (error: any) {
+      if (error.name === 'NotAllowedError') {
+        console.error('User declined the authentication request');
+      } else {
+        console.error('WebAuthn authentication error:', error);
+      }
     }
   }
 }
