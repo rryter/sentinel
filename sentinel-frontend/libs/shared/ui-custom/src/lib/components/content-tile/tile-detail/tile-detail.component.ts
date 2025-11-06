23a7cc1b43687b8e1359044f71b4deae87c96253
Reto Ryter <reto@twy.gmbh>
2025-11-06 16:15:54 +0000

    Use signal-based input for TileDetailComponent ⚡
    - Replace @Input property with signal-style input() for reactive inputs.
    - Update template to call icon() and adjust imports accordingly.
    - Remove unused CommonModule import and stray code cleanup.

--- sentinel-frontend/libs/shared/ui-custom/src/lib/components/content-tile/tile-detail/tile-detail.component.ts
+++ sentinel-frontend/libs/shared/ui-custom/src/lib/components/content-tile/tile-detail/tile-detail.component.ts
@@ -1,19 +1,17 @@
-import { HlmIcon } from '@spartan-ng/helm/icon';
-import { Component, Input } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { Component, input } from '@angular/core';
 import { NgIcon } from '@ng-icons/core';
+import { HlmIcon } from '@spartan-ng/helm/icon';
 
 @Component({
   selector: 'app-tile-detail',
-  imports: [CommonModule, NgIcon, HlmIcon],
+  imports: [NgIcon, HlmIcon],
   template: `
     <div class="flex items-center text-gray-500">
-      <ng-icon [name]="icon" hlm size="sm" class="mr-1"> </ng-icon>
+      <ng-icon [name]="icon()" hlm size="sm" class="mr-1"> </ng-icon>
       <span><ng-content></ng-content></span>
     </div>
   `,
 })
 export class TileDetailComponent {
-  @Input() icon: string = 'lucideInfo';
+  icon = input<string>('lucideInfo');
 }
-g;
